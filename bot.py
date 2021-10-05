206b0ff10a848223d2960c85053eb019636af18f
Mdmovies <91911991+Mdmovies@users.noreply.github.com>
2021-10-05 23:15:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,14 +14,14 @@ import pyrogram
 
 
 
-if name == "md-rename-bot" :
+if __name__ == "__main__" :
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(
         root="plugins"
     )
     app = pyrogram.Client(
-        "md-Rename-Bot",
+        "RenameBot",
         bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
