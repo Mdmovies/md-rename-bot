75d6c19d401e18cd9497a6d713f481b6d4e9950b
Mdmovies <91911991+Mdmovies@users.noreply.github.com>
2021-10-06 13:05:13 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,24 +1,47 @@
-class Translation(object):
-    START_TEXT = """ <b> Hi {} , \nI'm A Simple m.d File Renamer+File To Video Converter Bot With Permanent Thumbnail And Custom Caption support!</b> \n
+import os
 
-<b>Bot Maintained By: @mdadmin2 </b> \n 
-"""
+class Config(object):
+    # get a token from @BotFather
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    # The Telegram API things
+    APP_ID = int(os.environ.get("APP_ID", 12345))
+    API_HASH = os.environ.get("API_HASH")
+    # Update channel for Force Subscribe
+    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
+    # log channel
+    #LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "")
+    # Get these values from my.telegram.org
+    CHAT_ID = os.environ.get("CHAT_ID", "")
+    # Array to store users who are authorized to use the bot
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    # Banned Unwanted Members..
+    BANNED_USERS = []
+    # the download location, where the HTTP Server runs
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+    # Telegram maximum file upload size
+    MAX_FILE_SIZE = 50000000
+    TG_MAX_FILE_SIZE = 2097152000
+    FREE_USER_MAX_FILE_SIZE = 50000000
+    # chunk size that should be used with requests
+    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
+    # default thumbnail to be used in the videos
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
+    # proxy for accessing youtube-dl in GeoRestricted Areas
+    # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
+    HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
+    # https://t.me/hevcbay/951
+    OUO_IO_API_KEY = ""
+    # maximum message length in Telegram
+    MAX_MESSAGE_LENGTH = 4096
+    # set timeout for subprocess
+    PROCESS_MAX_TIMEOUT = 3600
+    # watermark file
+    DEF_WATER_MARK_FILE = ""
+    # Database url
+    DB_URI = os.environ.get("DATABASE_URL", "")
+    
 
-    BANNED_USER_TEXT = "Sorry!! But Mai Owner As Banned You From Using This Service. That Means You Can't Run Meh Now! \n \n Contact : @mdadmin2 For more Details.. " 
-    DOWNLOAD_START = "<b>Downloading To My server !! Pls Wait</b>"
-    UPLOAD_START = "<b>Downloading Completed Now I'm Uploading Into TeleGram</b>"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Thank you for Using Me Support Our Channel @Malayalamdubbedmoviegroup ❤️</b>"
-    SAVED_CUSTOM_THUMB_NAIL = "<b>thumbnail Saved ✅ This Is Permanent Until</b> /delthumb ❤"
-    DEL_ETED_CUSTOM_THUMB_NAIL = "thumbnail cleared succesfully🤦"
-    SAVED_RECVD_DOC_FILE = "<b>File Downloaded Successfully 😎</b>"
-    REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Please Reply To An File With /rename fil name extension To rename a file</b>"
-    REPLY_TO_FILE_FOR_CONVERT = "<b> Please Reply To An File With /c2v To Convert It Into Streamable video File</b>"
-    CUSTOM_CAPTION_UL_FILE = " "
-    NO_THUMB_FOUND = "No Thumbnail found"
-    IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
-    ABOUT_ME = """<b>Meh : \n An Telegram File Renamer Bot Which Can Rename A Telegram Files. \n You Can Set Permanent Thumbnail For The File So You Don't Have To Send Custom Thumbs All The Time.\n \n i can also convert files into videos \n Feedback @mdadmin2 If You Found Meh Useful</b>"""
-    HELP_USER = """Checkout The Available Commands Here \n\n Do Follow @Malayalamdubbedmoviegroup If You Find This Bot Usefull❤️"""
-    RENAME_HELP = """Here are The Available Commands In Reanme \n\n\n▪️ <code>/rename</code> : Reply To An File/video With <code>/rename Filename.extension</code> For Renaming"""
-    C2V_HELP = """Here Are The Available Commands In File To Video \n\n\n ▪️<code>/c2v</code> : Reply To An File With /c2v To Convert It Into Video"""
-    THUMBNAIL_HELP = """Here Are The Available Commands In Custom Thumbnail \n\n\n ▪️ Send A Photo To Set The Custom Thumbnail \n▪️ <code>/showthumb</code> : For Checking The Current Thumbnail \n▪️<code>/delthumb</code> : For Deleting The Current Saved Thumbnail"""
-    CCAPTION_HELP = """Here Are The Available Commands In Custom Caption \n\n\n ▪️<code>/scaption</code> Use This Command To Save Your Custom Caption \n<b>Usage:</b> <code>/scaption your caption text</code> \n\n<b>[You Can Use</b> <code>{filename}</code> <b>For showing new file name in the caption]</b> """
+
+
+
+
